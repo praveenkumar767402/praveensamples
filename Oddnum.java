cbbea210a362ef9386f4bd38def83995d0b733e1
praveenkumar767402 <142496908+praveenkumar767402@users.noreply.github.com>
2023-09-01 15:00:34 +0530

    Add files via upload

---
+++ Oddnum.java
@@ -0,0 +1,17 @@
+package xyz;
+
+public class Oddnum {
+
+	public static void main(String[] args) {
+		int j;
+		System.out.println("The First 10 Odd Numbers are");
+		for(int i=1; i<=10;i++) 
+		{
+			j=2*i-1;
+			System.out.println(j);
+		}
+	}
+
+	}
+
+
